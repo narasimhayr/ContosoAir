e785b8d6674c06070d4fc9782e42d1098b564496
Yannabathina Narasimharao <yannabathina.n@HCL.COM>
2019-07-17 14:47:00 +0530

    Changes airport sorting. Fixes AB#4

--- src/services/airports.service.js
+++ src/services/airports.service.js
@@ -7,9 +7,6 @@ class AirportsService {
         this._airports = airports;
     }
 
-   /* getAll(){
-        return this._airports.filter(a => a.code).map(avoidEmptyCity);
-    }*/
 
     getAll(){
         return this._airports.filter(a => a.code).map(avoidEmptyCity).sort((a, b) => (a.city > b.city) ? 1 : -1);
